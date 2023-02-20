73b24b5d1c4d4b7a3778b13ac6a0a3e8db6e23d2
Radiirgummii <git@radiirgummii.net>
2023-02-20 02:58:57 -0800

    added 003 listen und dicts

---
+++ Python/003_aufgaben_listen/001_listen.py
@@ -0,0 +1,7 @@
+wortliste = [ " Monitor " , " T a s t a t u r " , "Maus " , " Drucker " , "WLAN" ]
+print (wortliste [ 1 ] )
+AnzahlWoerter = len (wortliste)
+print (AnzahlWoerter)
+meinText = " G r a f i k k a r te , Prozessor , A r be i t s s pe iche r , SSD "
+neueListe= meinText.split( " , " )
+print (neueListe)
---
+++ Python/003_aufgaben_listen/002_dicts.py
@@ -0,0 +1,11 @@
+#Erzeugung eines Dictionarys
+alter = {"Horst": 42, "Günther": 24, "Dieter": 44, "Edwin": 22}
+print(alter)
+#Ersetzen eines Wertes
+alter["Edwin"] = 23
+print(alter)
+#Ein Element hinzufügen
+alter["Julia"] = 33
+print(alter)
+#Ausgabe
+print("Julia:", alter["Julia"])
